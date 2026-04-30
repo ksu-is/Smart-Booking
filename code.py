f8b809476934e88717ffb9454c3403a9f4f6e68f
Nicole Valencia <lvalenc5@students.kennesaw.edu>
2026-04-29 21:00:37 -0400

    Update code.py

--- code.py
+++ code.py
@@ -5,7 +5,13 @@ from datetime import datetime
 
 class booking 
 
-  
+def __init__(self, title, date, consultant):
+        self.title = title
+        self.date  = date
+        self.consultant = consultant
+
 def find_date
+ return self.date
 
 def find_consultant 
+return self.consultant
