04cc0eaf1ac1548b0dfd6275d258fbd4d9f01095
Nicole Valencia <lvalenc5@students.kennesaw.edu>
2026-05-09 16:47:45 -0400

    Update code.py

--- code.py
+++ code.py
@@ -11,7 +11,17 @@ def __init__(self, title, date, consultant):
         self.consultant = consultant
         
  # double booking 
+def show_booking(self):
+        print("Appointment: {self.title}")
+        print("Date: {self.date}")
+        print("Consultant: {self.consultant}")
 
+# Create appointment
+booking1 = Booking(
+    "Hair Appointment",
+    "05/10/2026",
+    "Clarissa"
+        
 def add_booking(self, new_booking):
  for booking in self.bookings:
  if booking.consultant == new_booking.consultant:
